385eb7c6523cbd31080ff765e7c8a9ea51b6a585
Hanumanthraya <hanumanthraya.0107@gmail.com>
2022-05-12 21:28:58 +0530

    upload file

---
+++ UploadFilesServer/UploadFilesServer/services/IUploadService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UploadFilesServer.services
+{
+    interface IUploadService
+    {
+        Task<string> UploadAsync(Stream fileStream, string fileName, string contentType);
+
+    }
+}
